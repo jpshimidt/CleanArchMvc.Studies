2e503bbdad75e6114cab889590c381bfb8b47c00
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-10-28 00:32:59 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArch.Mvc.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArch.Mvc.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
