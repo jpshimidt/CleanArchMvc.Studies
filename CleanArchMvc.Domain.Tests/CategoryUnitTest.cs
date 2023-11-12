0cd8d3fc4f93cca4e6b28e41d3147d3da7c7b2a7
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-11-12 03:31:18 -0300

    Added CategoryUnitTest

---
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class CategoryUnitTest
+    {
+        [Fact(DisplayName = "Create Category With Valid State")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Category With Invalid State")]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+
+        [Fact(DisplayName = "Create Category With Invalid Name")]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 charecters");
+        }
+
+        [Fact(DisplayName = "Create Category With Invalid Name")]
+        public void CreateCategory_NegativeIdValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact(DisplayName = "Create Category With Invalid State")]
+        public void CreateCategory_WithInvalidParameters_ResultObjectInvalidState()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>();
+        }
+    }
+}
--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -28,7 +28,7 @@ namespace CleanArchMvc.Domain.Entities
         private void ValidationDomain(string name)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name),
-                                            "Invalid name.Name is required");
+                                            "Invalid name. Name is required");
 
             DomainExceptionValidation.When(name.Length <= 3,
                                             "Invalid name, too short, minimum 3 charecters");
