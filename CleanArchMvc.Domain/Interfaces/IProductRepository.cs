1a37629734561904fcebb0bcbe3ad282fe52b801
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-11-12 03:06:34 -0300

    Create ICategoryRepository and IProductRepository

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetById(int? Id);
+        Task<Product> GetProductCategory(int? Id);
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(Product product);
+    }
+}
