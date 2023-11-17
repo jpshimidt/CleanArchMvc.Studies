48d0376de0ca91724587b118ebf8b1225601baf8
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-11-17 00:31:22 -0300

    Implement CategoryRepository and ProductRepository

---
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        readonly ApplicationDbContext _productContext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            _productContext.Add(product);
+            await _productContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> GetById(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategory(int? id)
+        {
+            return await _productContext.Products.Include(c => c.Category)
+                                                 .SingleOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> Remove(Product product)
+        {
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}
